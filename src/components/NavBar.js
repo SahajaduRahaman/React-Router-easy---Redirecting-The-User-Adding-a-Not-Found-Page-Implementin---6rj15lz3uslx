6b8a4561c87bf599962f84d118c46f74d52d02f2
Raza <sahajadurahaman@gmail.com>
2023-02-03 20:29:41 +0530

    assignment solved and working properly

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 
 export const NavBar = () =>{
     return(
-
+        <>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="/" class="index-link">index</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/home" class="home-link">home</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </>
     )
 }
